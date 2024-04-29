367bb6acd027c78362f5bf3dd99f9bd0ae5dd919
Ivan Merkuchev <ewanmerkuchev@yandex.ru>
2024-04-29 08:55:22 +0300

    add class Service

---
+++ Service.h
@@ -0,0 +1,16 @@
+//
+// Created by STRANGE on 29.04.2024.
+//
+
+#ifndef WEATHERSERVICE_SERVICE_H
+#define WEATHERSERVICE_SERVICE_H
+
+#include <string>
+#include "Weather.h"
+
+class Service {
+    virtual Weather getWeather(std::string s) = 0;
+    virtual ~Service() = 0;
+};
+
+#endif //WEATHERSERVICE_SERVICE_H
