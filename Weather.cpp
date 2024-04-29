fb6f67b248235fd38b814362e79ea5a549d6574b
Ivan Merkuchev <ewanmerkuchev@yandex.ru>
2024-04-29 08:44:33 +0300

    init git

---
+++ Weather.cpp
@@ -0,0 +1,5 @@
+//
+// Created by STRANGE on 29.04.2024.
+//
+
+#include "Weather.h"
---
+++ Weather.h
@@ -0,0 +1,14 @@
+//
+// Created by STRANGE on 29.04.2024.
+//
+
+#ifndef WEATHERSERVICE_WEATHER_H
+#define WEATHERSERVICE_WEATHER_H
+
+
+class Weather {
+
+};
+
+
+#endif //WEATHERSERVICE_WEATHER_H
